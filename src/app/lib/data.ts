6a4d0d0f6d264f0ecdfb8a2bd411d15d426f7ca6
huydang-teq <huydang@teqnological.asia>
2023-10-31 14:59:34 +0700

    test sever side

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -12,7 +12,7 @@ import { formatCurrency } from "./utils";
 import { unstable_noStore as noStore } from "next/cache";
 
 export async function fetchRevenue() {
-  // noStore();
+  noStore();
 
 
   try {
@@ -34,7 +34,7 @@ export async function fetchRevenue() {
 }
 
 export async function fetchLatestInvoices() {
-  // noStore();
+  noStore();
   try {
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
@@ -55,7 +55,7 @@ export async function fetchLatestInvoices() {
 }
 
 export async function fetchCardData() {
-  // noStore();
+  noStore();
   try {
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
